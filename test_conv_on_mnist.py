22f68ee963ff2148721ce54cafaf6960837ce915
Sebastian Scherer <Sebastian Scherer>
2018-05-06 12:09:32 +0100

    added (unverified) testing scripts

---
+++ test_conv_on_mnist.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 11:55:22 2018
+
+@author: bettmensch
+"""
+
+# This script tests 
+# - the fully connected
+# - the dropout
+# - the convolutional
+# - the conv. to fc. reshape
+# layer classes from the deep learning library classes by building a
+# classification model, training it on the mnist data set and evaluating
+# its performance
+
+# [1] imports from library and python lbs
+# [2] Build convolitional network
+# [3] Train convolutional network on mnist
+# [4] Evaluate convolutional network on mnist test data
+
+  
+# ---------------------------------
+# [1] Imports
+# ---------------------------------
+
+import numpy as np
+from mnist_classes import get_mnist_data
+from diy_deep_learning_library import FFNetwork
+
+# ---------------------------------
+# [2] Build convolitional network
+# ---------------------------------
+
+# get data
+X_train, X_test, y_train, y_test = get_mnist_data()
+
+print('Shape of X_train:',X_train.shape)
+print('Shape of y_train:',y_train.shape)
+
+X_train, X_test = np.array(X_train).reshape(-1,1,28,28),np.array(X_test).reshape(-1,1,28,28)
+y_train, y_test = y_train.reshape(-1,1), y_test.reshape(-1,1)
+
+print('Shape of X_train:',X_train.shape)
+print('Shape of y_train:',y_train.shape)
+
+# build network
+neuralNet = FFNetwork(2)
+
+kernelSize1 = 5
+channels1 = 6
+stride1 = 1
+padding1 = 'valid'
+
+poolingSize2 = 2
+stride2 = 2
+padding2 = 'valid'
+
+kernelSize3 = 5
+channels3 = 16
+stride3 = 1
+padding3 = 'valid'
+
+poolingSize4 = 2
+stride4 = 2
+padding4 = 'valid'
+
+n5 = 400
+
+n6 = 84
+
+n7 = 10
+
+neuralNet.addConvLayer(kernelHeight=kernelSize1,
+                       kernelWidth=kernelSize1,
+                       channels=channels1,
+                       stride=stride1,
+                       padding=padding1,
+                       activation='tanh')
+neuralNet.addPoolingLayer(poolingHeight=poolingSize2,
+                       poolingWidth=poolingSize2,
+                       stride=stride2,
+                       padding=padding2,
+                       poolingType='max')
+neuralNet.addConvLayer(kernelHeight=kernelSize3,
+                       kernelWidth=kernelSize3,
+                       channels=channels3,
+                       stride=stride3,
+                       padding=padding3,
+                       activation='tanh')
+neuralNet.addPoolingLayer(poolingHeight=poolingSize4,
+                       poolingWidth=poolingSize4,
+                       stride=stride4,
+                       padding=padding4,
+                       poolingType='max')
+
+neuralNet.addConvToFCReshapeLayer(n5)
+
+neuralNet.addFCLayer(n6,activation='tanh')
+
+neuralNet.addFCLayer(n7,activation='softmax')
+
+neuralNet.fixateNetwork(X_train[:10,:,:,:])
+
+print('\n ')
+print(neuralNet)
+
+# ---------------------------------
+# [3] Build convolitional network
+# ---------------------------------
+
+# train network
+nEpochs = 1
+learningRate = 0.5
+batchSize = 50
+displayStep = 10
+oneHotY = True
+
+neuralNet.trainNetwork(nEpochs,learningRate,batchSize,X_train,y_train,displayStep,oneHotY)
+
+# ---------------------------------
+# [4] Evaluate convolutional network on mnist test data
+# ---------------------------------
+
+# evaluate trained model
+PTrain = neuralNet.predict(X_train[:5000])
+PTest = neuralNet.predict(X_test[:2000])
+
+accuracyTrain = np.sum(PTrain == y_train[:5000]) / len(PTrain)
+accuracyTest = np.sum(PTest == y_test[:2000]) / len(PTest)
+
+print('Accuracy on training set:',accuracyTrain)
+print('Accuracy on test set:',accuracyTest)
---
+++ test_fc_on_mnist.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 11:45:34 2018
+
+@author: bettmensch
+"""
+
+# This script tests the fully connected and dropout layer classes from the 
+# deep learning library classes by building a
+# classification model, training it on the mnist data set and evaluating
+# its performance
+
+# [1] imports from library and python lbs
+# [2] Build fullly connected net
+# [3] Train fully connected net on mnist data set
+# [4] Evaluate fully connected net on mnist data test set
+
+# ---------------------------------
+# [1] Imports
+# ---------------------------------
+
+import numpy as np
+from mnist_classes import get_mnist_data
+from diy_deep_learning_library import FFNetwork
+
+# ---------------------------------
+# [2] Build fullly connected net
+# ---------------------------------
+
+# get data
+X_train, X_test, y_train, y_test = get_mnist_data()
+
+X_train, X_test = np.array(X_train), np.array(X_test)
+y_train, y_test = y_train.reshape(-1,1), y_test.reshape(-1,1)
+
+# build network
+neuralNet = FFNetwork()
+
+n1 = 300
+dropoutRate1 = 0.5
+n2 = 300
+dropoutRate2 = 0.5
+n3 = 10
+
+neuralNet.addFCLayer(n1,activation='tanh')
+neuralNet.addDropoutLayer(dropoutRate1)
+neuralNet.addFCLayer(n2,activation='tanh')
+neuralNet.addDropoutLayer(dropoutRate2)
+neuralNet.addFCLayer(n3,activation='softmax')
+
+neuralNet.fixateNetwork(X_train[:10,:])
+
+print(neuralNet)
+
+# ---------------------------------
+# [3] Train fully connected net on mnist data set
+# ---------------------------------
+
+# train network
+nEpochs = 10
+learningRate = 0.5
+batchSize = 50
+displayStep = 50
+oneHotY = True
+
+neuralNet.trainNetwork(nEpochs,learningRate,batchSize,X_train,y_train,displayStep,oneHotY)
+
+# ---------------------------------
+# [4] Evaluate fully connected net on mnist data test set
+# ---------------------------------
+
+# evaluate trained model
+PTrain = neuralNet.predict(X_train)
+PTest = neuralNet.predict(X_test)
+
+accuracyTrain = np.sum(PTrain == y_train) / len(PTrain)
+accuracyTest = np.sum(PTest == y_test) / len(PTest)
+
+print('Accuracy on training set:',accuracyTrain)
+print('Accuracy on test set:',accuracyTest)
